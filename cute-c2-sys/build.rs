3b97ca9ae211c8abc3b651b5eb751a44b95fe405
Ross <yeahross@protonmail.com>
2020-08-27 19:14:53 +0100

    Move cute header inside cute-c2-sys

--- cute-c2-sys/build.rs
+++ cute-c2-sys/build.rs
@@ -4,8 +4,7 @@ use std::{env, path::PathBuf};
 fn main() {
     let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let root_dir = project_dir.parent().unwrap();
-    let src = root_dir.join("cute-header");
+    let src = project_dir.join("cute-header");
 
     Build::new()
         .file(src.join("cute_c2.c"))
